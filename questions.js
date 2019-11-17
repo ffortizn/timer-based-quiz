87bdc444c1e79d32d21e81e6b7d313d36bfe17c2
Fernando Nicolas <ffortizn@gmail.com>
2019-11-16 19:16:07 -0800

    first commit

---
+++ questions.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Which Australian marsupial enjoy eating eucalyptus leaves?",
+        choices: ["koala", "dingo", "kangaroo", "wombat"],
+        answer: "koala"
+    },
+    {
+        title: "What is one quarter of 1000?",
+        choices: ["1000", "125", "250", "4000"],
+        answer: "250"
+    },
+    {
+        title: "What is the world's longest river? ",
+        choices: ["nile", "congo", "amazon", "yellow"],
+        answer: "amazon"
+    },
+     {
+         title: "Name the world's largest ocean",
+         choices: ["atlantic", "indian", "arctic", "pacific"],
+         answer: "pacific"
+    }
+
+
+    // {
+    //     title: "",
+    //     choices: ["", "", "", ""],
+    //     answer: ""
+    // },
+];
